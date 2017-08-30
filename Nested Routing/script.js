11125839dd9b0abbc18707b9a8d7161c9ea8a8c1
Theajinkya <ajinkya.chanshetty@gmail.com>
2017-08-30 23:56:48 +0530

    Added the Nested Routing

---
+++ Nested Routing/script.js
@@ -0,0 +1,23 @@
+var app = angular.module("myApp", [])
+
+//function myController($scope){
+//
+//$scope.person ="ARC"
+    
+    //var person = new Object();
+//person.firstName = "Sharif";
+//person.lastName = "Malik";
+//person.age = 24;
+//person.city = “Pune”;
+    
+//}
+
+
+app.controller("myCtrl", function($scope){
+    
+    $scope.firstName = "Ajinkya"
+    $scope.lastName = "Chanshetty"
+    $scope.contact = 9960282703
+    $scope.email = "aajinkya@hotmail.com"
+    
+})
